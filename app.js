0f8bdc41678bdeffc8ba6e1bd5fbbca6f6e91474
Kally <kallyasmedia@gmail.com>
2020-07-24 13:50:20 +0000

    Add display Images function

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-const API_URL = Process.env.API_URL;
+// require("dotenv").config();
+
+const API_URL = `https://pixabay.com/api`;
+const API_KEY = `17631253-060c608ba0a8d5a2779c2e3dc`;
+const BASE_URL = `${API_URL}/?key=${API_KEY}&q=`;
+
+console.log(API_URL);
+console.log(BASE_URL);
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const loadingImage = document.querySelector("#loadingImage");
+const imageSection = document.querySelector(".images");
+
+loadingImage.style.display = "none";
+form.addEventListener("submit", formSubmitted);
+
+function formSubmitted(e) {
+  e.preventDefault();
+  const searchTerm = input.value;
+  searchStart();
+  search(searchTerm)
+    .then(displayImages)
+    .then(() => {
+      loadingImage.style.display = "none";
+    });
+}
+
+function searchStart() {
+  loadingImage.style.display = "";
+  imageSection.innerHTML = "";
+}
+
+async function search(searchTerm) {
+  const url = `${BASE_URL}${searchTerm}&image_type=photo&pretty=true`;
+  console.log(url);
+  const response = await fetch(url);
+  const results = await response.json();
+  console.log(results);
+  return results.imageUrl;
+}
+
+function displayImages(results) {
+  result.forEach((result) => {
+    const imageElement = document.createElement("img");
+    imageElement.src = result.imageUrl[0];
+    imageSection.appendChild(imageElement);
+  });
+}
---
+++ config.js
@@ -0,0 +1,3 @@
+const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+const BASE_URL = `${API_URL}/?key=${API_KEY}&q=`;
