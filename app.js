ba9c68050046837bd6fc3712ed4f58f88b3af316
Kally <60466044+kallyas@users.noreply.github.com>
2021-09-18 13:28:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,46 +4,45 @@ const API_URL = `https://pixabay.com/api`;
 const API_KEY = `17631253-060c608ba0a8d5a2779c2e3dc`;
 const BASE_URL = `${API_URL}/?key=${API_KEY}&q=`;
 
-console.log(API_URL);
-console.log(BASE_URL);
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const loadingImage = document.querySelector('#loadingImage');
+const imageSection = document.querySelector('.images');
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const loadingImage = document.querySelector("#loadingImage");
-const imageSection = document.querySelector(".images");
+loadingImage.style.display = 'none';
 
-loadingImage.style.display = "none";
-form.addEventListener("submit", formSubmitted);
+form.addEventListener('submit', formSubmitted);
 
-function formSubmitted(e) {
-  e.preventDefault();
+function formSubmitted(event) {
+  event.preventDefault();
   const searchTerm = input.value;
+  
   searchStart();
   search(searchTerm)
     .then(displayImages)
     .then(() => {
-      loadingImage.style.display = "none";
+      loadingImage.style.display = 'none';
     });
 }
 
 function searchStart() {
-  loadingImage.style.display = "";
-  imageSection.innerHTML = "";
+  loadingImage.style.display = '';
+  imageSection.innerHTML = '';
 }
 
-async function search(searchTerm) {
-  const url = `${BASE_URL}${searchTerm}&image_type=photo&pretty=true`;
-  console.log(url);
-  const response = await fetch(url);
-  const results = await response.json();
-  console.log(results);
-  return results.imageUrl;
+function search(searchTerm) {
+  const url = `{BASE_URL}${searchTerm}&image_type=photo&pretty=true`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      return result.photos;
+    });
 }
 
-function displayImages(results) {
-  result.forEach((result) => {
-    const imageElement = document.createElement("img");
-    imageElement.src = result.imageUrl[0];
+function displayImages(images) {
+  images.forEach(image => {
+    const imageElement = document.createElement('img');
+    imageElement.src = image.image_url[0];
     imageSection.appendChild(imageElement);
   });
 }
